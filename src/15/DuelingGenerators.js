4280961cdb626dc0ec0458912e821f3f1800ad9a
Jeremy Berglund <jtberglund@gmail.com>
2018-01-22 22:14:29 -0800

    Day 15 using actual generators

--- src/15/DuelingGenerators.js
+++ src/15/DuelingGenerators.js
@@ -1,41 +1,23 @@
 import R from 'ramda';
 import { toBinary } from '../utils';
 
-const substrLastN = R.curry((n, str) => str.substr(str.length - n));
-
-const getValueToCompare = R.compose(substrLastN(16), toBinary);
-
-const compareGenerators = (generatorA, generatorB) => {
-    const valA = getValueToCompare(generatorA.value);
-    const valB = getValueToCompare(generatorB.value);
-    // console.log(toBinary(generatorA.value), toBinary(generatorB.value));
-    return valA === valB;
-};
-
-const generate = generator => {
-    // const { factor, value } = generator;
-    // const prevVal = values.length === 0 ? startingValue : R.last(values);
-    // console.log('prevVal', prevVal);
-    // const newVal = (generator.value * generator.factor) % 2147483647;
-    // generator.values.push(newVal);
-    generator.value = (generator.value * generator.factor) % 2147483647;
-    // return { ...generator, values: values.concat(newVal), factor };
-};
-
-const generatorFactory = (value, factor) => ({ value, factor });
+function* generate(value, factor) {
+    while (true) {
+        yield (value * factor) % 2147483647;
+    }
+}
 
 export const duelingGeneratorsPart1 = (startingValueA, startingValueB, numPairs) => {
-    const generatorA = generatorFactory(startingValueA, 16807);
-    const generatorB = generatorFactory(startingValueB, 48271);
-
+    let valA = startingValueA;
+    let valB = startingValueB;
     let count = 0;
+    const mask = 0b00000000000000001111111111111111;
     for (let i = 0; i < numPairs; i++) {
-        generate(generatorA);
-        generate(generatorB);
-        if (compareGenerators(generatorA, generatorB)) {
+        valA = generate(valA, 16807).next().value
+        valB = generate(valB, 48271).next().value
+        if ((valA & mask) === (valB & mask)) {
             count += 1;
         }
-        // count = compareGenerators(generatorA, generatorB) ? count + 1 : count;
     }
     return count;
 };
--- src/15/DuelingGenerators.test.js
+++ src/15/DuelingGenerators.test.js
@@ -1,7 +1,7 @@
 import { duelingGeneratorsPart1 } from './DuelingGenerators';
 
 describe('Dueling Generators Part 1', () => {
-    test.only('Example', () => {
+    test('Example', () => {
         const startingValueA = 65;
         const startingValueB = 8921;
         expect(duelingGeneratorsPart1(startingValueA, startingValueB, 5)).toBe(1);
@@ -16,6 +16,6 @@ describe('Dueling Generators Part 1', () => {
     test('Puzzle input', () => {
         const startingValueA = 591;
         const startingValueB = 393;
-        expect(duelingGeneratorsPart1(startingValueA, startingValueB, 40000000)).toBe(618);
+        expect(duelingGeneratorsPart1(startingValueA, startingValueB, 40000000)).toBe(619);
     });
 });
