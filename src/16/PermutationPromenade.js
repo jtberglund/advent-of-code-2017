dde94b4e68a95f9dddf5e4b6e960f3242f268935
Berglund <jberglund@integrity-apps.com>
2018-01-23 13:26:40 -0800

    Day 16 WIP

--- src/16/PermutationPromenade.js
+++ src/16/PermutationPromenade.js
@@ -1,5 +1,46 @@
 import R from 'ramda';
 
-export const permutationPromenadePart1 = () => {
-    //
+export const spin = (numPrograms, programs) => R.compose(R.flatten, R.reverse)(R.splitAt(programs.length - numPrograms, programs));
+
+export const exchange = (index1, index2, programs) => {
+    if (index1 < 0 || index2 < 0 || index1 > programs.length - 1 || index2 > programs.length - 1) {
+        return programs;
+    }
+
+    const val1 = programs[index1];
+    const val2 = programs[index2];
+    return R.compose(R.set(R.lensIndex(index1), val2), R.set(R.lensIndex(index2), val1))(programs);
+};
+
+export const partner = (program1, program2, programs) => {
+    const index1 = programs.indexOf(program1);
+    const index2 = programs.indexOf(program2);
+    return exchange(index1, index2, programs);
+};
+
+const INSTRUCTION_FNS = {
+    s: spin,
+    x: exchange,
+    p: partner
+};
+
+const getArgsForInstruction = instruction => {
+    const args = R.tail(instruction).split('/');
+    return args.map(char => parseInt(char, 10) || char);
+};
+
+const getFunctionForInstruction = instruction => {
+    return R.propOr(R.identity, R.head(instruction), INSTRUCTION_FNS);
+};
+
+export const parseInstruction = instruction => {
+    const fn = getFunctionForInstruction(instruction);
+    const args = getArgsForInstruction(instruction);
+    // console.log(`Args for ${instruction}: ${args}`);
+    return program => fn(...args, program);
+};
+
+export const permutationPromenadePart1 = (instructions, programs) => {
+    const instructionList = R.map(parseInstruction, instructions).slice(0, 1000);
+    return R.pipe(R.split(''), ...instructionList, R.join(''))(programs);
 };
--- src/16/PermutationPromenade.test.js
+++ src/16/PermutationPromenade.test.js
@@ -1,7 +1,85 @@
-import { permutationPromenadePart1 } from './PermutationPromenade';
+import { exchange, partner, permutationPromenadePart1, spin } from './PermutationPromenade';
+
+import { readFile } from '../utils';
+
+const PUZZLE_INPUT = readFile(`${__dirname}/input.txt`);
+
+describe('spin', () => {
+    const start = ['a', 'b', 'c', 'd', 'e'];
+
+    test('Handles out of bounds inputs', () => {
+        expect(spin(-1, start)).toEqual(start);
+    });
+
+    test('Inputs greater than array length should wrap', () => {
+        expect(spin(6, start)).toEqual(spin(1, start));
+    });
+
+    test('spin(0, [a,b,c,d,e]) produces abcde', () => {
+        expect(spin(0, start)).toEqual(start);
+    });
+
+    test('spin(1, [a,b,c,d,e]) produces eabcd', () => {
+        expect(spin(1, start)).toEqual(['e', 'a', 'b', 'c', 'd']);
+    });
+
+    test('spin(3, [a,b,c,d,e]) produces cdeab', () => {
+        expect(spin(3, start)).toEqual(['c', 'd', 'e', 'a', 'b']);
+    });
+
+    test('spin(5, [a,b,c,d,e]) produces cdeab', () => {
+        expect(spin(5, start)).toEqual(start);
+    });
+});
+
+describe('exchange', () => {
+    const start = ['a', 'b', 'c', 'd', 'e'];
+
+    test('Handles out of bounds input', () => {
+        expect(exchange(-1, 0, start)).toEqual(start);
+        expect(exchange(10, 0, start)).toEqual(start);
+    });
+
+    test('exchange(0, 0, [a,b,c,d,e]) produces abcde', () => {
+        expect(exchange(0, 0, start)).toEqual(start);
+    });
+
+    test('exchange(0, 4, [a,b,c,d,e]) produces ebcda', () => {
+        expect(exchange(0, 4, start)).toEqual(['e', 'b', 'c', 'd', 'a']);
+    });
+
+    test('exchange(1, 3, [a,b,c,d,e]) produces adcbe', () => {
+        expect(exchange(1, 3, start)).toEqual(['a', 'd', 'c', 'b', 'e']);
+    });
+});
+
+describe('partner', () => {
+    const start = ['a', 'b', 'c', 'd', 'e'];
+
+    test('Handles non-existent elements', () => {
+        expect(partner('z', 'a', start)).toEqual(start);
+        expect(partner(-'p', 'q', start)).toEqual(start);
+    });
+
+    test('partner("a", "b", [a,b,c,d,e]) produces bacde', () => {
+        expect(partner('a', 'b', start)).toEqual(['b', 'a', 'c', 'd', 'e']);
+    });
+
+    test('partner("a", "e", [a,b,c,d,e]) produces ebcda', () => {
+        expect(partner('a', 'e', start)).toEqual(['e', 'b', 'c', 'd', 'a']);
+    });
+});
 
 describe('Permutation Promenade Part 1', () => {
-    test('', () => {
-        //
+    test('Example', () => {
+        const instructions = ['s1', 'x3/4', 'pe/b'];
+        expect(permutationPromenadePart1(instructions, 'abcde')).toEqual('baedc');
+    });
+
+    test.only('Puzzle input', () => {
+        const programs = 'abcdefghijklmnop';
+        return PUZZLE_INPUT.then(instructions => instructions.split(',')).then(instructions => {
+            return expect(permutationPromenadePart1(instructions, programs)).toEqual('');
+        });
     });
 });
