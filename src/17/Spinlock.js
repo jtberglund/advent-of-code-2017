a75b2bc759b95bd16d73a96d1ea2efc92172e80f
Berglund <jberglund@integrity-apps.com>
2018-01-24 10:37:22 -0800

    Day 17 part 1

--- src/17/Spinlock.js
+++ src/17/Spinlock.js
@@ -1,5 +1,24 @@
 import R from 'ramda';
 
-export const spinlockPart1 = () => {
-    //
+const getIndexCircular = (index, arr) => index % arr.length;
+
+const insert = (index, val, arr) => [...arr.slice(0, index), val, ...arr.slice(index, arr.length)];
+
+const stepOnce = (numSteps, val, { index, buffer }) => {
+    const nextIndex = getIndexCircular(index + numSteps, buffer) + 1;
+    return { index: nextIndex, buffer: insert(nextIndex, val, buffer) };
 };
+
+export const step = (numSteps, numIterations) =>
+    R.reduce(
+        (state, i) => stepOnce(numSteps, i + 1, state),
+        {
+            index: 0,
+            buffer: [0]
+        },
+        R.range(0, numIterations)
+    );
+
+const getNextBufferValue = ({ index, buffer }) => buffer[index + 1];
+
+export const spinlockPart1 = R.compose(getNextBufferValue, step);
--- src/17/Spinlock.test.js
+++ src/17/Spinlock.test.js
@@ -1,7 +1,39 @@
-import { spinlockPart1 } from './Spinlock';
+import { spinlockPart1, step } from './Spinlock';
 
 describe('Spinlock Part 1', () => {
-    test('', () => {
-        //
+    test('Step 1 time', () => {
+        expect(step(3, 1)).toEqual({
+            index: 1,
+            buffer: [0, 1]
+        });
+    });
+
+    test('Step 2 times', () => {
+        expect(step(3, 2)).toEqual({
+            index: 1,
+            buffer: [0, 2, 1]
+        });
+    });
+
+    test('Step 3 times', () => {
+        expect(step(3, 3)).toEqual({
+            index: 2,
+            buffer: [0, 2, 3, 1]
+        });
+    });
+
+    test('Step 4 times', () => {
+        expect(step(3, 4)).toEqual({
+            index: 2,
+            buffer: [0, 2, 4, 3, 1]
+        });
+    });
+
+    test('Example', () => {
+        expect(spinlockPart1(3, 2017)).toBe(638);
+    });
+
+    test('Puzzle input', () => {
+        expect(spinlockPart1(304, 2017)).toBe(1173);
     });
 });
