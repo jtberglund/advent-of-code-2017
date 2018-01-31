b3f5ffcbaf88687b024e1c33cc76ef9f7ef30af0
Berglund <jberglund@integrity-apps.com>
2018-01-31 11:05:08 -0800

    Day 20 part 2

--- src/20/ParticleSwarm.js
+++ src/20/ParticleSwarm.js
@@ -30,3 +30,38 @@ export const particleSwarmPart1 = particleList => {
     const sorted = sortParticles(particles);
     return particles.indexOf(R.head(sorted));
 };
+
+const coordToString = ({ x, y, z }) => `${x},${y},${z}`;
+
+const markPosition = (acc, pos) => ({
+    ...acc,
+    [pos]: (acc[pos] || 0) + 1
+});
+
+/**
+ * Removes all particles that have collided (positions are equal)
+ */
+const filterCollisions = particles => {
+    const positions = R.map(R.prop('p'), particles)
+        .map(coordToString)
+        .reduce(markPosition, {});
+    return R.filter((particle, i) => positions[coordToString(particle.p)] === 1, particles);
+};
+
+export const particleSwarmPart2 = particleList => {
+    let particles = R.map(parseParticle, particleList);
+
+    const MIN_ITERATIONS = 50;
+    let lastLength = 0;
+    let iter = 0;
+    while (true) {
+        particles = R.map(update, particles);
+        particles = filterCollisions(particles);
+        if (particles.length === lastLength && iter >= MIN_ITERATIONS) {
+            break;
+        }
+        lastLength = particles.length;
+        iter++;
+    }
+    return lastLength;
+};
--- src/20/ParticleSwarm.test.js
+++ src/20/ParticleSwarm.test.js
@@ -1,4 +1,4 @@
-import { parseParticle, particleSwarmPart1, update } from './ParticleSwarm';
+import { parseParticle, particleSwarmPart1, particleSwarmPart2, update } from './ParticleSwarm';
 
 import { readFileLineByLine } from '../utils';
 
@@ -44,13 +44,39 @@ describe('Particle Swarm Part 1', () => {
     ];
 
     test('Example', () => {
-        expect(particleSwarmPart1(EXAMPLE)).toEqual(0);
+        const actual = particleSwarmPart1(EXAMPLE);
+        const expected = 0;
+        expect(actual).toEqual(expected);
     });
 
     test('Puzzle input', () => {
         return PUZZLE_INPUT.then(particles => {
             const actual = particleSwarmPart1(particles);
             const expected = 157;
+            return expect(actual).toEqual(expected);
+        });
+    });
+});
+
+describe('Particle Swarm Part 2', () => {
+    // prettier-ignore
+    const EXAMPLE = [
+        'p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>',
+        'p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>',
+        'p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>',
+        'p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>'
+    ];
+
+    test('Example', () => {
+        const actual = particleSwarmPart2(EXAMPLE);
+        const expected = 1;
+        expect(actual).toEqual(expected);
+    });
+
+    test('Puzzle input', () => {
+        return PUZZLE_INPUT.then(particles => {
+            const actual = particleSwarmPart2(particles);
+            const expected = 499;
             return expect(actual).toEqual(expected);
         });
     });
