3fc0584f4b15a7ebe314f37be69c92a9799a430d
Jeremy Berglund <jtberglund@gmail.com>
2018-01-31 09:49:57 -0800

    Day 20 part 1

--- src/20/ParticleSwarm.js
+++ src/20/ParticleSwarm.js
@@ -1,5 +1,32 @@
+import { strToInt, trace } from '../utils';
+
 import R from 'ramda';
 
-export const particleSwarmPart1 = () => {
-    //
+// 'p=<1,2,3>' -> '1,2,3'
+const stripBrackets = str => str.substr(3, str.length - 4);
+// 'p=<1,2,3>' -> { x: 1, y: 2, z: 3 }
+const parseVector = R.compose(([x, y, z]) => ({ x, y, z }), R.map(strToInt), R.split(','), stripBrackets);
+
+export const parseParticle = R.compose(([p, v, a]) => ({ p, v, a }), R.map(parseVector), R.split(', '));
+
+const magnitude = ({ x, y, z }) => Math.abs(x) + Math.abs(y) + Math.abs(z);
+
+const add = ({ x: x1, y: y1, z: z1 }, { x: x2, y: y2, z: z2 }) => ({ x: x1 + x2, y: y1 + y2, z: z1 + z2 });
+
+export const update = particle => {
+    const v = add(particle.v, particle.a);
+    return { p: add(particle.p, v), v, a: particle.a };
+};
+
+// Sorts by acceleration, then velocity, then position
+const sortParticles = R.sortWith([
+    R.ascend(R.compose(magnitude, R.prop('a'))),
+    R.ascend(R.compose(magnitude, R.prop('v'))),
+    R.ascend(R.compose(magnitude, R.prop('p')))
+]);
+
+export const particleSwarmPart1 = particleList => {
+    const particles = R.map(parseParticle, particleList);
+    const sorted = sortParticles(particles);
+    return particles.indexOf(R.head(sorted));
 };
--- src/20/ParticleSwarm.test.js
+++ src/20/ParticleSwarm.test.js
@@ -1,7 +1,57 @@
-import { particleSwarmPart1 } from './ParticleSwarm';
+import { parseParticle, particleSwarmPart1, update } from './ParticleSwarm';
+
+import { readFileLineByLine } from '../utils';
+
+const PUZZLE_INPUT = readFileLineByLine(`${__dirname}/input.txt`);
+
+describe('parseParticle', () => {
+    test('Correctly parses a particle string to an object', () => {
+        const particle = 'p=<3,0,1>, v=<2,-1,-1>, a=<-1,10,-20>';
+        const actual = parseParticle(particle);
+        const expected = {
+            p: { x: 3, y: 0, z: 1 },
+            v: { x: 2, y: -1, z: -1 },
+            a: { x: -1, y: 10, z: -20 }
+        };
+        expect(actual).toEqual(expected);
+    });
+});
+
+describe('update', () => {
+    test('Increases velocity before position', () => {
+        const particle = {
+            p: { x: 1, y: 1, z: 1 },
+            v: { x: 1, y: 1, z: 1 },
+            a: { x: 1, y: 1, z: 1 }
+        };
+
+        const actual = update(particle);
+        const expected = {
+            p: { x: 3, y: 3, z: 3 },
+            v: { x: 2, y: 2, z: 2 },
+            a: { x: 1, y: 1, z: 1 }
+        };
+
+        expect(actual).toEqual(expected);
+    });
+});
 
 describe('Particle Swarm Part 1', () => {
+    // prettier-ignore
+    const EXAMPLE = [
+        'p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>',
+        'p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>'
+    ];
+
     test('Example', () => {
-        //
+        expect(particleSwarmPart1(EXAMPLE)).toEqual(0);
+    });
+
+    test('Puzzle input', () => {
+        return PUZZLE_INPUT.then(particles => {
+            const actual = particleSwarmPart1(particles);
+            const expected = 157;
+            return expect(actual).toEqual(expected);
+        });
     });
 });
