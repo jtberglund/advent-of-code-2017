74157c2afeae0a8901a60926852bc019d5ea893c
Berglund <jberglund@integrity-apps.com>
2018-02-02 15:53:30 -0800

    WIP barely touched day 21

--- src/21/FractalArt.js
+++ src/21/FractalArt.js
@@ -1,5 +1,28 @@
 import R from 'ramda';
 
-export const fractalArtPart1 = () => {
-    //
+const ON = '#';
+const OFF = '.';
+
+const flip = R.map(R.reverse);
+const rotateClockwise = R.transpose;
+const rotateCounterClockwise = R.compose(R.transpose, flip);
+
+// prettier-ignore
+const BASE_GRID = [
+    [OFF, ON, OFF],
+    [OFF, OFF, ON],
+    [ON, ON, ON]
+];
+
+const testRule = (rule, grid) => {
+    // TODO
+};
+
+const parseRule = rule => {
+    // TODO
+};
+
+export const fractalArtPart1 = (ruleList, numIterations = 1) => {
+    const rules = R.map(parseRule, ruleList);
+    // TODO
 };
--- src/21/FractalArt.test.js
+++ src/21/FractalArt.test.js
@@ -1,4 +1,4 @@
-import { fractalArtPart1 } from './FractalArt';
+import { fractalArtPart1, rotate90 } from './FractalArt';
 
 describe('Fractal Art Part 1', () => {
     test('', () => {
