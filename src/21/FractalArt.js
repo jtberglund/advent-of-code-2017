d3a8114727c4c6888fee53a982359aeb52ba3ad2
Berglund <jberglund@integrity-apps.com>
2018-02-05 15:10:19 -0800

    (WIP) Day 21 added rule-matcher

--- src/21/FractalArt.js
+++ src/21/FractalArt.js
@@ -1,11 +1,12 @@
+import Maybe from 'folktale/maybe';
 import R from 'ramda';
 
 const ON = '#';
 const OFF = '.';
 
 const flip = R.map(R.reverse);
-const rotateClockwise = R.transpose;
-const rotateCounterClockwise = R.compose(R.transpose, flip);
+const rotateClockwise = R.compose(R.transpose, flip);
+const rotateCounterClockwise = R.transpose;
 
 // prettier-ignore
 const BASE_GRID = [
@@ -14,15 +15,65 @@ const BASE_GRID = [
     [ON, ON, ON]
 ];
 
-const testRule = (rule, grid) => {
+// [['.', '#'], ['#', '.']] -> '.#/#.'
+const gridToStr = R.compose(R.join('/'), R.map(R.join('')));
+// '.#/#.' -> [['.', '#'], ['#', '.']]
+const strToGrid = R.compose(R.map(R.split('')), R.split('/'));
+
+const getMatch = (rules, grid) => {
+    return rules[gridToStr(grid)] || rules[flip(gridToStr(grid))];
+};
+
+export const apply = (rules, grid) => {
+    let newGrid;
+    for (let i = 0; i < 4; i++) {
+        newGrid = getMatch(rules, grid);
+        if (newGrid) {
+            return Maybe.of(strToGrid(newGrid));
+        }
+
+        // Rotate the grid clockwise each time to try a different permutation
+        grid = rotateClockwise(grid);
+    }
+    return Maybe.empty();
+};
+
+const dividesBy = R.curry((divisor, arr) => arr.length & (divisor === 0));
+const dividesByTwo = dividesBy(2);
+const dividesByThree = dividesBy(3);
+
+const divideBy = (amount, grid) => {};
+
+export const divideGrid = grid => {
+    let newGrid;
+    if (dividesByTwo(grid)) {
+        newGrid = divideBy(2, grid);
+    }
+    if (dividesByThree(grid)) {
+        newGrid = divideBy(3, grid);
+    }
+
+    //
+};
+
+const joinGrids = grids => {
     // TODO
 };
 
 const parseRule = rule => {
-    // TODO
+    const [key, _, value] = rule.split(' ');
+    return { [key]: value };
 };
 
+const parseRules = R.compose(R.reduce((acc, rule) => ({ ...acc, ...rule }), {}), R.map(parseRule));
+
 export const fractalArtPart1 = (ruleList, numIterations = 1) => {
-    const rules = R.map(parseRule, ruleList);
-    // TODO
+    const rules = parseRules(ruleList);
+
+    let result = BASE_GRID;
+    for (let i = 0; i < numIterations; i++) {
+        result = apply(rules, result).getOrElse();
+    }
+
+    return result;
 };
--- src/21/FractalArt.test.js
+++ src/21/FractalArt.test.js
@@ -1,7 +1,85 @@
-import { fractalArtPart1, rotate90 } from './FractalArt';
+import { apply, fractalArtPart1 } from './FractalArt';
+
+// prettier-ignore
+describe('apply rules', () => {
+
+    const rules = { '../.#': '##./#../...' };
+    const expected = [
+        ['#', '#', '.'],
+        ['#', '.', '.'],
+        ['.', '.', '.']
+    ];
+
+    test('Rules matches grid rotated 90deg', () => {
+        const grid = [
+            ['.', '.'],
+            ['#', '.']
+        ];
+
+        const actual = apply(rules, grid).getOrElse();
+        expect(actual).toEqual(expected);
+    });
+
+    test('Rules matches grid rotated 90deg flipped', () => {
+        const grid = [
+            ['.', '.'],
+            ['.', '#']
+        ];
+
+        const actual = apply(rules, grid).getOrElse();
+        expect(actual).toEqual(expected);
+    });
+
+    test('Rules matches grid rotated 180deg', () => {
+        const grid = [
+            ['#', '.'],
+            ['.', '.']
+        ];
+
+        const actual = apply(rules, grid).getOrElse();
+        expect(actual).toEqual(expected);
+    });
+
+    test('Rules matches grid rotated 180deg flipped', () => {
+        const grid = [
+            ['.', '#'],
+            ['.', '.']
+        ];
+
+        const actual = apply(rules, grid).getOrElse();
+        expect(actual).toEqual(expected);
+    });
+});
 
 describe('Fractal Art Part 1', () => {
-    test('', () => {
-        //
+    test('Example - 1 iteration', () => {
+        const rules = ['../.# => ##./#../...', '.#./..#/### => #..#/..../..../#..#'];
+
+        const actual = fractalArtPart1(rules, 1);
+        // prettier-ignore
+        const expected = [
+            ['#','.','.','#',],
+            ['.','.','.','.',],
+            ['.','.','.','.',],
+            ['#','.','.','#',]
+        ];
+
+        expect(actual).toEqual(expected);
+    });
+    test('Example - 2 iterations', () => {
+        const rules = ['../.# => ##./#../...', '.#./..#/### => #..#/..../..../#..#'];
+
+        const actual = fractalArtPart1(rules, 2);
+        // prettier-ignore
+        const expected = [
+            ['#','#','.','#','#','.',],
+            ['#','.','.','#','.','.',],
+            ['.','.','.','.','.','.',],
+            ['#','#','.','#','#','.',],
+            ['#','.','.','#','.','.',],
+            ['.','.','.','.','.','.',]
+        ];
+
+        expect(actual).toEqual(expected);
     });
 });
